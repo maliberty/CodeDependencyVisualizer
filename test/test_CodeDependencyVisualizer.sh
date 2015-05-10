2ab91470dfa95c214062f0fbc3f0145fabe0b26c
Matt Liberty <matt.liberty@gmail.com>
2015-05-09 20:10:15 -0700

    Use 'open' on Mac for VIEWER (and minor formatting)

--- test/test_CodeDependencyVisualizer.sh
+++ test/test_CodeDependencyVisualizer.sh
@@ -3,7 +3,14 @@
 TMP_DIR="/tmp"
 CPP_SRC_DIR="dummyCppProject"
 CUML_ARGS="-aiptP --verbose -d ${CPP_SRC_DIR} -I ./dummyCppProject/subfolder -o ${TMP_DIR}/uml.dot --excludeClasses G"
-VIEWER="firefox"
+platform=`uname`
+if [[ "$platform" == 'Darwin' ]]; then
+   VIEWER="open"
+else
+   VIEWER="firefox"
+fi
 OUTPUT_FORMAT="svg"
 
-python2 ../src/CodeDependencyVisualizer.py ${CUML_ARGS} && dot -T ${OUTPUT_FORMAT} -o ${TMP_DIR}/uml.${OUTPUT_FORMAT} ${TMP_DIR}/uml.dot && ${VIEWER} ${TMP_DIR}/uml.${OUTPUT_FORMAT}
+python2 ../src/CodeDependencyVisualizer.py ${CUML_ARGS} \
+  && dot -T ${OUTPUT_FORMAT} -o ${TMP_DIR}/uml.${OUTPUT_FORMAT} ${TMP_DIR}/uml.dot \
+  && ${VIEWER} ${TMP_DIR}/uml.${OUTPUT_FORMAT}
--- test/test_DotGenerator.sh
+++ test/test_DotGenerator.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 TMP_DIR="/tmp"
-VIEWER="eog"
+platform=`uname`
+if [[ "$platform" == 'Darwin' ]]; then
+   VIEWER="open"
+else
+   VIEWER="eog"
+fi
 export PYTHONPATH=../src
 
-python2 test_DotGenerator.py ${TMP_DIR}/uml2.dot && dot -T png -o ${TMP_DIR}/uml2.png ${TMP_DIR}/uml2.dot && ${VIEWER} ${TMP_DIR}/uml2.png
+python2 test_DotGenerator.py ${TMP_DIR}/uml2.dot \
+  && dot -T png -o ${TMP_DIR}/uml2.png ${TMP_DIR}/uml2.dot \
+  && ${VIEWER} ${TMP_DIR}/uml2.png
